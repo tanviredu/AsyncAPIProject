213768dc0896cc0c13ab42d1e31a259e18708422
MD.Tanvir Rahman <143000410@eastdelta.edu.bd>
2021-11-09 18:07:09 +0600

    Add files via upload

---
+++ Books.Application/Services/BookRepository.cs
@@ -0,0 +1,47 @@
+﻿using Books.Domain;
+using Books.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books.Application.Services
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly BookContext _context;
+        public BookRepository(BookContext context)
+        {
+            _context = context;
+        }
+
+     
+
+        public void AddBook(Book bookToAdd)
+        {
+            if (bookToAdd == null) {
+
+                throw new ArgumentNullException(nameof(bookToAdd));
+
+            }
+            _context.Add(bookToAdd);
+        }
+
+        public async Task<Book> GetBookByIdAsync(int Id)
+        {
+            return await _context.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == Id);
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksAsync()
+        {
+            return await _context.Books.Include(b=>b.Author).ToListAsync();
+        }
+
+        public async Task<bool> SaveChangeAsync()
+        {
+            return (await _context.SaveChangesAsync() > 0);
+        }
+    }
+}
